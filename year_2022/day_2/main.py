04de03da4819a6819a29df8d22e7d06c427ff527
OisinWard <oisin.l.ward@gmail.com>
2023-07-09 12:42:23 +0000

    Adding calculations for each option

--- year_2022/day_2/main.py
+++ year_2022/day_2/main.py
@@ -34,5 +34,19 @@ with open('input.txt','r') as input_file:
             playerMoveTranslated = "Paper"
         if playerMove == "Z":
             playerMoveTranslated = "Scisors"
+"""        
+A X 1+3=4    Rock Rock = Draw
+A Y 1+6=7    Rock Paper = Win
+A Z 1+0=1    Rock Scisors = Loss 
+B X 2+0=2    Paper Rock = Loss
+B Y 2+3=5    Paper Paper = Draw
+B Z 2+6=8    Paper Scisors = Win
+C X 3+6=9    Scisors Rock = Win
+C Y 3+0=3    Scisors Paper = Loss
+C Z 3+3=6    Scisors Scisors = Draw
+"""         
 
+        if playerMoveTranslated == opponentMoveTranslated:
+            result = "Draw"
+        if playerMoveTranslated 
         print(f"opponentMoveTranslated: {opponentMoveTranslated} playerMoveTranslated: {playerMoveTranslated}")
