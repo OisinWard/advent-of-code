e3744ea460f6595413f5f528aee5d05a7dcf1798
OisinWard <oisin.l.ward@gmail.com>
2023-12-03 21:55:38 +0000

    Cleaning up code

--- year_2023/day_2/main.py
+++ year_2023/day_2/main.py
@@ -6,10 +6,8 @@ sum_part_1 = 0
 # Code for part 1 
 with open('input.txt','r') as input_file:
     for line in input_file:
-       if((re.search(r'(1[3-9] red|1[4-9] green|1[5-9] blue|[2-9][0-9] red|[2-9][0-9] green|[2-9][0-9] blue)', line))): 
-           pass
-       else:
+       if not ((re.search(r'(1[3-9] red|1[4-9] green|1[5-9] blue|[2-9][0-9] red|[2-9][0-9] green|[2-9][0-9] blue)', line))): 
            sum_part_1 = line_num + sum_part_1
        line_num += 1
-       print( sum_part_1 )
+    print( sum_part_1 )
        
