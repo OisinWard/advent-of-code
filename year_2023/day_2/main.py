71257039561b54e76b878824987a75252dad3e09
OisinWard <oisin.l.ward@gmail.com>
2023-12-03 21:49:22 +0000

    First working solution part 1

---
+++ year_2023/day_2/main.py
@@ -0,0 +1,15 @@
+import re
+
+line_num = 1
+sum_part_1 = 0
+
+# Code for part 1 
+with open('input.txt','r') as input_file:
+    for line in input_file:
+       if((re.search(r'(1[3-9] red|1[4-9] green|1[5-9] blue|[2-9][0-9] red|[2-9][0-9] green|[2-9][0-9] blue)', line))): 
+           pass
+       else:
+           sum_part_1 = line_num + sum_part_1
+       line_num += 1
+       print( sum_part_1 )
+       
